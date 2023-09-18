4ef8f3e19b50f24e8949da6fd64045d8534ba548
Ngan LE <ngan.ltkn2911@gmail.com>
2023-09-18 15:23:56 +0200

    added values for db_airbnb

--- db_airbnb.sql
+++ db_airbnb.sql
@@ -22,6 +22,18 @@ CREATE TABLE location(
 	location_image VARCHAR(150)
 );
 
+INSERT INTO location (location_name, province, nation, location_image)
+VALUES 	('Paris', 'Île-de-France', 'France'),
+		('Phan Thiet', 'Binh Thuan', 'Vietnam'),
+		('Nha Trang', 'Khanh Hoa', 'Vietnam'),
+		('Nakano City', 'Tokyo', 'Japan'),
+		('Long Xuyen', 'An Giang', 'Vietnam'),
+		('Nice', 'Provence-Alpes-Côte d'Azur, 'France'),
+		('Peshastin', 'Washington', 'United States'),
+		('County Wicklow', 'Ireland', 'Ireland'),
+		('Colares', 'Lisboa', 'Portugal'),
+		('Brando', 'Corse', 'France');
+
 CREATE TABLE rooms(
 	room_id INT PRIMARY KEY AUTO_INCREMENT,
 	room_name VARCHAR(250),
@@ -43,6 +55,12 @@ CREATE TABLE rooms(
 	FOREIGN KEY (location_id) REFERENCES location(location_id),
 	image VARCHAR(250)
 );
+
+INSERT INTO rooms (room_name, client_number, bed_room, bed, bath_room, description, price, washing_machine, iron, tivi, air_conditioner, wifi, kitchen, parking, pool, location_id)
+VALUES	('Casa Acqua Erbalunga', 2, 1, 1, 1, 'The dream for a stay for two! 800m from the village of Erbalunga, a quiet place, a magnificent sea and mountain view, just what it takes to spend pleasant moments. A mini private pool, heated to 30 degrees from November to May, a covered terrace sheltered from heat. An exotic atmosphere for a perfect relaxation! 90m2 designed for laziness! 
+The loft and villa are never rented at the same time.', 215, true, false, true, false, true, true, true, false, 10), 
+		('Coconut Garden Villa', 4, 1, 2, 1, 'Just 100 meters from the sea, Coconut Garden offers comfortable and intimate 70sqm duplex bungalows. The pool and the tropical garden make Coconut Garden a little haven of peace. On the ground floor, facing the garden and pool, a spacious and pleasant room awaits you. Guests can enjoy AC and a flat-screen TV with cable channels.', 21, true, true, true, true, true, true, true, true, 2), 
+		('Le Nest Hotel', 2, 1, 1, 1, 'The Superior Room is in a monochrome blue and green. There are a couple of versions, but each is carefully decorated with selected elegant accessories and stylish colours to capture the essence of the surrounding area. Quality bedding, soft and harmonious colours, a marble bathroom that opens like a wardrobe, independent toilets, a television nestled in the fireplace, Baumann 1950 chairs, vintage mirror, are the assets of this superior room.', 173, false, false, true, false, true, true, true, false, 1); 
 
 CREATE TABLE reservations(
 	reservation_id INT PRIMARY KEY AUTO_INCREMENT,
