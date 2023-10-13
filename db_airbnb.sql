09ef5fe3e4a093da0ee117503a99bf0399656449
Ngan LE <ngan.ltkn2911@gmail.com>
2023-10-13 16:12:11 +0200

    modified db_airbnb

--- db_airbnb.sql
+++ db_airbnb.sql
@@ -22,7 +22,7 @@ CREATE TABLE location(
 	location_image VARCHAR(150)
 );
 
-INSERT INTO location (location_name, province, nation, location_image)
+/* INSERT INTO location (location_name, province, nation, location_image)
 VALUES 	('Paris', 'Ile-de-France', 'France'),
 		('Phan Thiet', 'Binh Thuan', 'Vietnam'),
 		('Nha Trang', 'Khanh Hoa', 'Vietnam'),
@@ -32,7 +32,7 @@ VALUES 	('Paris', 'Ile-de-France', 'France'),
 		('Peshastin', 'Washington', 'United States'),
 		('County Wicklow', 'Ireland', 'Ireland'),
 		('Colares', 'Lisboa', 'Portugal'),
-		('Brando', 'Corse', 'France');
+		('Brando', 'Corse', 'France'); */
 
 CREATE TABLE rooms(
 	room_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -56,10 +56,10 @@ CREATE TABLE rooms(
 	image VARCHAR(250)
 );
 
-INSERT INTO rooms (room_name, client_number, bed_room, bed, bath_room, description, price, washing_machine, iron, tivi, air_conditioner, wifi, kitchen, parking, pool, location_id)
+I/* NSERT INTO rooms (room_name, client_number, bed_room, bed, bath_room, description, price, washing_machine, iron, tivi, air_conditioner, wifi, kitchen, parking, pool, location_id)
 VALUES	('Casa Acqua Erbalunga', 2, 1, 1, 1, 'The dream for a stay for two!', 215, true, false, true, false, true, true, true, false, 10), 
 		('Coconut Garden Villa', 4, 1, 2, 1, 'Just 100 meters from the sea, Coconut Garden offers comfortable and intimate 70sqm duplex bungalows.', 21, true, true, true, true, true, true, true, true, 2), 
-		('Le Nest Hotel', 2, 1, 1, 1, 'The Superior Room is in a monochrome blue and green.', 173, false, false, true, false, true, true, true, false, 1); 
+		('Le Nest Hotel', 2, 1, 1, 1, 'The Superior Room is in a monochrome blue and green.', 173, false, false, true, false, true, true, true, false, 1); */ 
 
 CREATE TABLE reservations(
 	reservation_id INT PRIMARY KEY AUTO_INCREMENT,
